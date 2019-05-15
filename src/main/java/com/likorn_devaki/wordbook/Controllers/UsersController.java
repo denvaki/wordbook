dcacc141f7d30d1409909fe1efb276756f1cc5ce
Paktalin <likorn@ttu.ee>
2019-05-15 11:36:20 +0300

    Broken registration/auth

--- src/main/java/com/likorn_devaki/wordbook/Controllers/UsersController.java
+++ src/main/java/com/likorn_devaki/wordbook/Controllers/UsersController.java
@@ -2,11 +2,14 @@ package com.likorn_devaki.wordbook.Controllers;
 
 import com.likorn_devaki.wordbook.Entities.Token;
 import com.likorn_devaki.wordbook.Entities.User;
-import com.likorn_devaki.wordbook.PasswordEncoder.PasswordEncoder;
 import com.likorn_devaki.wordbook.Repos.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -20,28 +23,43 @@ public class UsersController {
 
     @Autowired
     private UsersRepo usersRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @PostMapping(path = "register")
     public ResponseEntity createUser(@RequestBody User user) {
-        if(user.getUsername() ==  null || user.getPassword() == null){
+        if (user.getUsername() == null || user.getPassword() == null) {
             return ResponseEntity.badRequest().body("Empty one or both of credentials fields");
         }
         user.setCreated(LocalDateTime.now());
-        if (usersRepo.existsByUsername(user.getUsername())){
-            return ResponseEntity.badRequest().body(String.format("Username %s already exist",  user.getUsername()));
+        if (usersRepo.existsByUsername(user.getUsername())) {
+            return ResponseEntity.badRequest().body(String.format("Username %s already exist", user.getUsername()));
         }
-        user.setPassword(PasswordEncoder.encode(user.getPassword()));
+        String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
         usersRepo.save(user);
+
         return ResponseEntity.status(HttpStatus.CREATED).build();
 
     }
 
     @PostMapping(path = "login")
-    public Token loginUser(@RequestBody User user) {
+    public String loginUser(@RequestBody User user) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+
+
         User dbUser = usersRepo.findUserByUsernameAndPassword(user.getUsername(), user.getPassword());
+        // TODO check that the password is correct
+        String token = null;
         if (dbUser != null)
-            return new Token(user);
-        return  null;
+            //TODO encrypt the token with the private(?) key
+            token = new Token(dbUser).toString();
+        System.out.println(token);
+        return token;
     }
 
     @GetMapping(path = "all_users")
