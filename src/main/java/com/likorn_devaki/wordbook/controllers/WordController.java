4b3002777fdff60295ce6c5969b0e9880ffa5aae
Paktalin <likorn@ttu.ee>
2019-05-19 01:45:31 +0300

    Added HttpServletRequest to all the Words requests

--- src/main/java/com/likorn_devaki/wordbook/controllers/WordController.java
+++ src/main/java/com/likorn_devaki/wordbook/controllers/WordController.java
@@ -19,12 +19,18 @@ public class WordController {
     private WordsService wordsService;
 
     @PostMapping(path = "save_word")
-    public Word save(@RequestBody Word word){
+    public Word save(@RequestBody Word word, HttpServletRequest request){
+        String token = extractToken(request);
+        if (token == null)
+            return null;
         return wordsService.save(word);
     }
 
     @GetMapping("{id}")
-    public Word findOne(@PathVariable Integer id) {
+    public Word findOne(@PathVariable Integer id, HttpServletRequest request) {
+        String token = extractToken(request);
+        if (token == null)
+            return null;
         return wordsService.findOne(id);
     }
 
@@ -33,23 +39,32 @@ public class WordController {
             @RequestParam(value = "foreign_word", required = false) String foreignWord,
             @RequestParam(value = "translated_word", required = false) String translatedWord,
             @RequestParam(value = "tag", required = false) String tag,
-            HttpServletRequest req) {
-        String token = JWTProvider.resolveToken(req).orElse(null);
-        if (JWTProvider.invalidToken(token)){
+            HttpServletRequest request) {
+        String token = extractToken(request);
+        if (token == null)
             return ResponseEntity.badRequest().body("Bad credentials");
-        }
         List<Word> usersWords = wordsService.findAllByUsername(foreignWord, translatedWord, tag, JWTProvider.getUsername(token));
         return ResponseEntity.ok(usersWords);
     }
 
     @PutMapping(path = "update_word")
-    public Word update(@RequestParam(value = "word_id") Integer wordId, @RequestBody Word word) {
+    public Word update(@RequestParam(value = "word_id") Integer wordId, @RequestBody Word word, HttpServletRequest request) {
         // TODO refactor the method to remove the wordId parameter
+        String token = extractToken(request);
+        if (token == null)
+            return null;
         return wordsService.update(wordId, word);
     }
 
     @DeleteMapping(path = "delete_word")
     public void delete(@RequestParam(value = "word_id") Integer wordId) {
         wordsService.delete(wordId);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String token = JWTProvider.resolveToken(request).orElse(null);
+        if (JWTProvider.invalidToken(token))
+            return null;
+        return token;
     }
 }
--- src/main/java/com/likorn_devaki/wordbook/f.java
+++
@@ -1,9 +0,0 @@
-package com.likorn_devaki.wordbook;
-
-import java.util.Date;
-
-public class f {
-    public static void main(String[] args) {
-        System.out.println(new Date());
-    }
-}
