6218eb0fe717ec2db439e3c61c16a370a5b99a22
Paktalin <likorn@ttu.ee>
2019-05-19 17:01:41 +0300

    secretKey is initialized as a field

--- src/main/java/com/likorn_devaki/wordbook/JWT/JWTProvider.java
+++ src/main/java/com/likorn_devaki/wordbook/JWT/JWTProvider.java
@@ -10,17 +10,11 @@ import java.util.Date;
 import java.util.Optional;
 
 public class JWTProvider {
-    private static SecretKey secretKey = null;
+    private static SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     private JWTProvider() {}
 
-    private static void init() {
-        if (secretKey == null)
-            secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    }
-
     public static String createJWT(Integer userId) {
-        init();
         return Jwts.builder()
                 .setIssuedAt(new Date())
                 .setSubject(String.valueOf(userId))
@@ -30,19 +24,16 @@ public class JWTProvider {
     }
 
     public static Integer getUserId(String token) {
-        init();
         return Integer.parseInt(Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject());
     }
 
     public static Optional<String> resolveToken(HttpServletRequest req) {
-        init();
         return Optional.ofNullable(req.getHeader("Authorization"))
                 .filter(header -> header.startsWith("Bearer "))
                 .map(header -> header.substring(7));
     }
 
     public static boolean invalidToken(String token) {
-        init();
         Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
         try {
             return claims.getBody().getExpiration().before(new Date());
